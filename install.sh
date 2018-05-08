0485e1b3c1dde21301e2a2370876449364b228be
sriman-biarca <srimanc@biarca.com>
2018-05-08 12:58:20 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,8 +13,5 @@ export LD_LIBRARY_PATH=/usr/lib/oracle/11.2/client64/lib/:$LD_LIBRARY_PATH
 export ORACLE_HOME=/usr/lib/oracle/11.2/client64
 export PATH=$PATH:$ORACLE_HOME/bin
 
-cd /usr/lib/oracle/11.2/client64
-ls -ltr
 
-
-"$ORACLE_HOME/bin/sqlplus" SYSTEM/biarca123@"104.197.220.94/XE" @'../sample.txt'
+"$ORACLE_HOME/bin/sqlplus" SYSTEM/biarca123@"104.197.220.94/XE" @'sample.txt'
